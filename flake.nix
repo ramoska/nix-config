a369543a25a1c6b8566ff94038ad1955736a6e0d
Irmantas Ramoška <i.ramoshka@gmail.com>
2025-12-26 17:20:15 +0200

    Drop Zen browser for NixOS until official support

--- flake.nix
+++ flake.nix
@@ -8,31 +8,18 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nixos-hardware, zen-browser, home-manager }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nixos-hardware }:
   {
     inherit nixpkgs;
 
     mbp = {
       imports = [
         nixos-hardware.nixosModules.apple-t2
-        home-manager.nixosModules.home-manager
         ./modules/common/all.nix
         ./modules/nixos/all.nix
-        ./modules/nixos/zen.nix
       ];
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.extraSpecialArgs = { inherit inputs; };
     };
 
     work = {
