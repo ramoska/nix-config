423b0b0f5bc418824d2a603cfb2cda98fb2d149f
Irmantas Ramoška <i.ramoshka@gmail.com>
2026-01-08 21:41:58 +0200

    Fix niri logout/login issues

--- dotfiles/dot_config/gammastep/hooks/executable_color-scheme-switch.sh
+++ dotfiles/dot_config/gammastep/hooks/executable_color-scheme-switch.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-notify-send "Gammastep Hook" "Event: $1\nOld: $2\nNew: $3" --icon=display-brightness-symbolic
-
-if [ "$1" = "period-changed" ] || [ "$1" = "initial" ]; then
+if [ "$1" = "period-changed" ]; then
   case "$3" in
     night)
       dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
       ;;
-    daytime|transition)
+    daytime)
       dconf write /org/gnome/desktop/interface/color-scheme "'prefer-light'"
       ;;
   esac
